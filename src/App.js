528ef8d25185ea52eb62c42119b68c188e214868
slroberts <shomariroberts@gmail.comclear>
2020-05-21 19:59:39 -0400

    Added Change Quater and reset buttons

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "./App.css";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [score, setScore] = useState({lions: 0, tigers: 0});
+  const [quater, setQuater] = useState(1);
 
   const handleScoreUpdate = (team, points) => {
     if (team === "lions") {
@@ -17,11 +18,22 @@ function App() {
     }
   };
 
+  const handleQuaterUpdate = () => {
+    if (quater < 4) {
+      setQuater(quater + 1);
+    }
+  };
+
+  const handleReset = () => {
+    setScore({lions: 0, tigers: 0});
+    setQuater(quater * 0 + 1);
+  };
+
   return (
     <div className="container">
       <section className="scoreboard">
         <ScoreBoard homeScore={score.lions} awayScore={score.tigers} />
-        <BottomRow />
+        <BottomRow quaterValue={quater} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -47,6 +59,19 @@ function App() {
             buttonClass={"awayButtons__fieldGoal"}
             buttonValue={"Away Field Goal"}
             handleClick={() => handleScoreUpdate("tigers", score.tigers + 3)}
+          />
+        </div>
+        <div>
+          <Button
+            buttonClass={"awayButtons__fieldGoal"}
+            buttonValue={"Change Quater"}
+            handleClick={() => handleQuaterUpdate()}
+          />
+
+          <Button
+            buttonClass={"awayButtons__fieldGoal"}
+            buttonValue={"Reset"}
+            handleClick={() => handleReset()}
           />
         </div>
       </section>
